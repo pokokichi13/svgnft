e5862ec2e211111af3728602fb9dc69b0b5c7b07
pokokichi13 <95128025+pokokichi13@users.noreply.github.com>
2021-12-14 18:50:23 +1100

    Update svgnft.sol

--- svgnft.sol
+++ svgnft.sol
@@ -7,26 +7,24 @@ import "https://github.com/Brechtpd/base64/blob/main/base64.sol";
 
 contract SVGtoNFT is ERC721URIStorage, Ownable {
     // Incremental counter of token ID and we iterate
-    uint256 public tokenCounter;
+    uint public tokenCounter;
+    
+    // Emits event on NFT create()
     event CreatedSVGNFT(uint256 indexed tokenId, string tokenURI);
     
     // Ad struct
     struct Ad {
 	address minter; // Whoever minted this NFT
-	uint256 price; // How much a sponsor is willing to pay for the ad
-	uint expiryDate; // When this ad expires
+	address landOwner; // Currentlty where the ad is
+	uint32 landx; // x cor of land
+	uint32 landy; // y cor of land
+	uint32 expiryDate; // When this ad expires
     }
-    // stores an `Ad` struct using tokenID as a key.
-    mapping(uint => Ad) public ads;
+    // stores an `Ad` struct. tokenID corresponds to the index.
+    Ad[] public ads;
     
-    // LandOwner struct
-    struct Landowner {
-	uint256 landx; // x cor of land
-	uint256 landy; // y cor of land
-	uint256[] tokenIDs; // ID of transferred NFTs
-    }
-    // stores a `LandOwner` struct for each address.
-    mapping(address => LandOwner) public landOwners;
+    // Setting expiry of ad
+    uint expiryDuration = 1 weeks;
     
     constructor() ERC721("SVG on-chain NFT", "svgNFT")
     {
@@ -37,17 +35,17 @@ contract SVGtoNFT is ERC721URIStorage, Ownable {
     // 1. SVG > Image URI
     // 2. Image URI > Token URI format
     // 3. Create with Token URI
-    function create(string memory svg, uint256 memory price) public {
+    function create(string memory svg) public {
         _safeMint(msg.sender, tokenCounter);
         // 1. SVG format > data:XXX
         string memory imageURI = svgToImageURI(svg);
         // 2. ERC721Metadata func to make it in token URI form
         _setTokenURI(tokenCounter, formatTokenURI(imageURI));
         
-        // Add to ads and set all values
-        Ad storage ad = ads[tokenCounter];
-        ad.price = price;
-        ad.minter = msg.sender;
+	// Add an ad to array
+	// Default land coordinate = 0,0
+	// Expiry is set to max of uint
+	ads.push(Ad(msg.sender, msg.sender, 0, 0, 4294967295));
         
         tokenCounter = tokenCounter + 1;
         emit CreatedSVGNFT(tokenCounter, svg);
@@ -79,5 +77,52 @@ contract SVGtoNFT is ERC721URIStorage, Ownable {
                     )
                 )
             );
+    }
+    
+    // Check expiry and return bool
+    function _expiryCheck(Ad storage _ad) internal view returns (bool) {
+      return (_ad.expiryDate <= now);
+    }
+    
+    // Show expiry of NFT
+    function getExpiry(uint memory _tokenID) internal view returns (uint32) {
+      return ads[_tokenID].expiryDate;
+    }
+    
+    // Send NFT from one to another
+    function sendNFT(address _to, uint memory _tokenID) external{
+        require(msg.sender == ads[_tokenID].minter,"Only minter can transfer");
+        // ERC721 function that transfers owner
+	transferFrom(msg.sender, _to, _tokenID);
+	// Change ads array info
+        ads[_tokenID].landOwner = _to;
+	ads[_tokenID].expiryDate = uint32(now + expiryDuration);
+	
+	//TBD
+	//Set land coordinate
+	
+        emit TransferredSVGNFT(_tokenID, to);
+    }
+    
+    // Check expiry and send the ad back to owner
+    function sendBackNFT(uint memory _tokenID) external{
+	    require(msg.sender == ads[_tokenID].minter,"Only minter can transfer");
+	    Ad storage myad = ads[_tokenID];
+	    require(_expiryCheck(myad));
+	    // Send back the NFT
+	    transferFrom(msg.sender, msg.sender, _tokenID);
+    }
+    
+    // Check NFts you have. Returns an array of tokenIDs 
+    function getMyNFT(address _myAddress) external view returns(uint[]){
+    	uint[] memory result = new uint[](ads.length);
+	uint counter = 0;
+    	for (uint i = 0; i < ads.length; i++) {
+    		if (ads[i].landOwner == _myAddress) {
+    			result[counter] = i;
+			counter++;
+		}
+	}
+	return result;
     }
 }
